3b4d4873d37723518acbf0c15b8cc5d92e56101a
Mayra Alzate <nadja.minni@gmail.com>
2025-08-13 18:31:36 -0500

    Agregar línea de separación en la clase Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,10 +6,12 @@ public class Main {
         // TODO: Crear método main para:
         // 1. Crear dos objetos Estudiante con diferentes valores
 
-        Estudiante estudiante1=new Estudiante("Manuel", 18, 4.5);
-        Estudiante estudiante2=new Estudiante("Sara", 19, 4.4);
-        
+        Estudiante estudiante1 = new Estudiante("Manuel", 18, 4.5);
+        Estudiante estudiante2 = new Estudiante("Sara", 19, 4.4);
+
         // 2. Mostrar la información de ambos estudiantes
+
+
 
         estudiante1.mostrarInformacion();
         estudiante2.mostrarInformacion();
@@ -24,15 +26,18 @@ public class Main {
         estudiante1.mostrarInformacion();
         estudiante2.mostrarInformacion();
 
+        System.out.println(
+                "=================================================================================================================================");
+
         // TODO: Crear método main para:
 
         // 1. Crear un libro usando el constructor completo
 
-       Libro libro1=new Libro("El amor en los tiempos del cólera","Gabriel García Márquez" , 60000);
+        Libro libro1 = new Libro("El amor en los tiempos del cólera", "Gabriel García Márquez", 60000);
 
         // 2. Crear un libro usando el constructor con solo titulo y autor
 
-        Libro libro2=new Libro("El visitante", "Stephen King");
+        Libro libro2 = new Libro("El visitante", "Stephen King");
 
         // 3. Mostrar información de ambos libros
 
@@ -40,7 +45,7 @@ public class Main {
         libro2.mostrarInformacion();
 
         // 4. Aplicar descuento usando ambas versiones del método
-
+        
         libro1.aplicarDescuento(10, 50000);
         libro2.aplicarDescuento(5);
 
